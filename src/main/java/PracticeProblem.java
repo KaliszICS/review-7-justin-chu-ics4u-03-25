a8b8679ca67b3872fccb5c8f3a215a2794da8856
justin-chu-ics4u-03-25 <justin.chu4@student.tdsb.on.ca>
2025-02-12 20:06:07 +0000

    My answers

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,81 @@ public class PracticeProblem {
 
 	}
 
-	//Write your functions here
+	public static String evenOrOdd(int hello) {
+		if (hello % 2 == 0) {
+			return "Even";
+		}
+
+		else {
+			return "Odd";
+		}
+	}
+
+	public static String teacherOrStudent (String hallo) {
+		if (hallo.equals("Kalisz")) {
+			return "Teacher";
+		}
+
+		else {
+			return "Student";
+		}
+	}
+	
+	public static int fartherFromZero (int baka) {
+		if (baka > 0) {
+			return baka + 5;
+		}
+
+		else {
+			if (baka < 0) {
+				return baka - 5;
+			}
+
+			else {
+				return 0;
+			}
+		}
+	}
+
+	public static String isFive(int isitfive) {
+		if (isitfive == 5) {
+			return "The number is Five";
+		}
+
+		else {
+			return "The number is not Five";
+		}
+	}
+
+	public static String positiveOrNegative(int positivenegative) {
+		if (positivenegative > 0) {
+			return "Positive";
+		}
+
+		else {
+			return "Negative";
+		}
+	}
+
+	public static String highOrLow(int overahunned) {
+		if (overahunned > 100) {
+			return "High";
+		}
+
+		else {
+			return "Low";
+		}
+	}
+
+	public static String isHello(String helloquestionmark) {
+		if (helloquestionmark.equals("Hello")) {
+			return "The word is Hello";
+		}
+
+		else {
+			return "The word is not Hello";
+		}
+	}
+
 
 }
